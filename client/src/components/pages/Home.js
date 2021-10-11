43f9988f660f2335ae2429ba0b692b19adde9362
Justin Bresee <justinbresee@gmail.com>
2021-10-11 09:53:05 -0600

    trying to fix live

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -11,7 +11,7 @@ function Home() {
     const getCurrentUser = () => {
         axios({
             method: 'GET',
-            url: API_URL + '/api/users/user',
+            url: API_URL + 'api/users/user',
             withCredentials: true
         }).then((data) => {
             console.log(data)
@@ -20,7 +20,7 @@ function Home() {
     }
 
     useEffect(() => {
-        axios.get(API_URL + '/api/users/hello')
+        axios.get(API_URL + 'api/users/hello')
             .then((res) => setMessage(res.data.msg))
             .catch((err) => console.log(err))
     })
