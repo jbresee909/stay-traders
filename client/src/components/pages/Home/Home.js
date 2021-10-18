b2f13bba5096c21a9dae01fefd3b212ee9554f2e
Justin Bresee <justinbresee@gmail.com>
2021-10-18 14:15:53 -0600

    created basic home page

--- client/src/components/pages/Home/Home.js
+++ client/src/components/pages/Home/Home.js
@@ -1,21 +1,27 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React from 'react';
+import "./Home.css"
+import { Link } from "react-router-dom";
 
 function Home(props) {
-    const [message, setMessage] = useState('')
+    if (props.currentUserFirstName) {
+        return (<div id="home-page">
+            <div id="home-hero">
+                <h1>Welcome, {props.currentUserFirstName}</h1>
+            </div>
+        </div>)
+    }
+    else {
+        return (
+            <div id="home-page">
+                <div id="home-hero">
+                    <div>
+                        <h1>Stay Traders</h1>
+                        <Link to="/register">Start Trading</Link>
+                    </div>
+                </div>
+            </div>)
+    }
 
-    useEffect(() => {
-        // Frontend check to check database connection
-        axios.get(process.env.REACT_APP_API + '/users/hello', { withCredentials: true })
-            .then((res) => setMessage(res.data.msg))
-            .catch((err) => console.log(err))
-    })
-
-    return (
-        <div>
-            <h1>Welcome, {props.currentUserFirstName}</h1>
-            <p>From the database: {message}</p>
-        </div>);
 }
 
 export default Home;
